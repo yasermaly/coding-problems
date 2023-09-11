29765860057ad6ada940093534ecdfe823952c5e
Yaser Aly <yaser-aly@live.com>
2023-09-10 19:05:44 -0700

    third problem solved

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ function addition(a, b) {
 }
 
 console.log(addition(3, 2));
+
+/*
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+*/
+
+// SOLUTION:
+
+function addition(a) {
+  const result = a + 1;
+  return result;
+}
+console.log(addition(9));
