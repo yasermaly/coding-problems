13c1df6db640c80134580368888de98b3d571ad8
Yaser Aly <yaser-aly@live.com>
2023-09-11 15:21:21 -0700

    problem 4 solved

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Write a function that takes an integer minutes and converts it to seconds.
-/* Examples
+/* Problem 1
+ Write a function that takes an integer minutes and converts it to seconds.
+Examples
 convert(5) ➞ 300
 
 convert(3) ➞ 180
@@ -15,7 +16,7 @@ function convert(x) {
 
 console.log(convert(5));
 
-/*
+/* Problem 2
 Create a function that takes two numbers as arguments and returns their sum.
 
 Examples
@@ -36,7 +37,7 @@ function addition(a, b) {
 
 console.log(addition(3, 2));
 
-/*
+/* Problem 3
 Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 
 Examples
@@ -54,3 +55,25 @@ function addition(a) {
   return result;
 }
 console.log(addition(9));
+
+/* Problem 4
+Write a function that takes the base and height of a triangle and return its area.
+
+Examples
+triArea(3, 2) ➞ 3
+
+triArea(7, 4) ➞ 14
+
+triArea(10, 10) ➞ 50
+Notes
+The area of a triangle is: (base * height) / 2
+Don't forget to return the result.
+*/
+
+// SOLUTION
+
+function triArea(base, height) {
+  const result = (base * height) / 2;
+  return result;
+}
+console.log(triArea(7, 4));
