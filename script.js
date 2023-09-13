deec9390104d2b7f6c11e75e8656437110a25cb1
Yaser Aly <yaser-aly@live.com>
2023-09-13 09:41:42 -0700

    problem 7 solved

--- script.js
+++ script.js
@@ -118,3 +118,22 @@ function lessThanOrEqualToZero(n) {
   }
 }
 console.log(lessThanOrEqualToZero(-2));
+
+/* Problem 7
+Create a function that takes length and width and finds the perimeter of a rectangle.
+
+Examples
+findPerimeter(6, 7) ➞ 26
+
+findPerimeter(20, 10) ➞ 60
+
+findPerimeter(2, 9) ➞ 22
+*/
+
+// SOLUTION
+
+function findPerimeter(l, w) {
+  const result = (l + w) * 2;
+  return result;
+}
+console.log(findPerimeter(10, 20));
